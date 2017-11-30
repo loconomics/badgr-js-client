7e1c9d841722cf35829ac11fbe13341d116a4345
Nolan Darilek <nolan@thewordnerd.info>
2017-11-30 11:14:34 -0600

    Refactor fetch wrapper and helper methods to functions outside of Client class.

--- src/index.ts
+++ src/index.ts
@@ -17,34 +17,33 @@ export interface UsernamePassword {
   password: string
 }
 
+async function myFetch(endpoint: string, args: object, method: HttpMethod = HttpMethod.Get) {
+  let fetchArgs = {method, ...args}
+  debug(`fetch("${endpoint}", ${JSON.stringify(fetchArgs)})`)
+  let r = await fetch(endpoint, fetchArgs)
+  if(r.ok)
+    return await r.json()
+  else
+    throw new Error(r.statusText)
+}
+
+async function get(endpoint: string, args: object) {
+  return myFetch(endpoint, args, HttpMethod.Get)
+}
+
+async function post(endpoint: string, args: object) {
+  return myFetch(endpoint, args, HttpMethod.Post)
+}
+
+async function put(endpoint: string, args: object) {
+  return myFetch(endpoint, args, HttpMethod.Put)
+}
+
+async function del(endpoint: string, args: object) {
+  return myFetch(endpoint, args, HttpMethod.Delete)
+}
+
 export default class {
-
-  private async fetch(endpoint: string, args: object, method: HttpMethod = HttpMethod.Get) {
-    let fetchArgs = {method, ...args}
-    debug(`fetch("${endpoint}", ${JSON.stringify(fetchArgs)})`)
-    let r = await fetch(endpoint, fetchArgs)
-    if(r.ok) {
-      let rv = await r.json()
-      return rv
-    } else
-      throw new Error(r.statusText)
-  }
-
-  private async get(endpoint: string, args: object) {
-    return this.fetch(endpoint, args, HttpMethod.Get)
-  }
-
-  private async post(endpoint: string, args: object) {
-    return this.fetch(endpoint, args, HttpMethod.Post)
-  }
-
-  private async put(endpoint: string, args: object) {
-    return this.fetch(endpoint, args, HttpMethod.Put)
-  }
-
-  private async delete(endpoint: string, args: object) {
-    return this.fetch(endpoint, args, HttpMethod.Delete)
-  }
 
   get apiEndpoint() {
     return `${this.endpoint}/v2`
@@ -60,7 +59,7 @@ export default class {
     const credentialsForm = new FormData()
     credentialsForm.append("username", credentials.username)
     credentialsForm.append("password", credentials.password)
-    this.post(this.authEndpoint, {body: credentialsForm})
+    post(this.authEndpoint, {body: credentialsForm})
       .then((token) => this.token = token["token"])
       .catch((e) => { throw e })
   }
